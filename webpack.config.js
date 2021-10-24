ecec54e2a2fa8e1f4b46b1e52c6e1cc10c73a811
tomw66 <tom.wilson96@hotmail.com>
2021-10-24 20:53:13 +0100

    initial set up, webpack and jest

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+    loaders: [
+        {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
+        {loader: 'style-loader!css-loader', test: /\.css$/},
+        {loader: 'url-loader', test: /\.gif$/},
+        {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+      ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
+    extensions: ['', 'js', 'jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
+},
+};
