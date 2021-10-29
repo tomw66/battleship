69d67153afc54ccbf7fc4d90eb728e9515bf301a
tomw66 <tom.wilson96@hotmail.com>
2021-10-29 12:01:46 +0100

    further tests

--- src/index.js
+++ src/index.js
@@ -10,21 +10,45 @@ const Ship = (name, length) => {
 };
 
 const Gameboard = () => {
-    //const boardGrid = Array.from(Array(10), () => new Array(10)) Is an overall grid actually needed?
-
+    const occupiedArray = [];
+    const missedArray = [];
     const placeShip = (name, length, startCoord, orientation) => {
         const ship = Ship(name, length);
         const createPositionArray = () => {
             const positionArray = [];
-            let dim;
-            let thing;
-            orientation === 'x' ? (dim = startCoord[0], thing = [i, startCoord[1]]) : (dim = startCoord[1], thing = [startCoord[0], i])
-            for(let i = dim; i < (dim + length); i++) {
-                positionArray.push(thing)
+            let dimension;
+            const coord = (or, x, y) => {
+                if(or ==='x') {return [x, y[1]]}
+                else if(or === 'y') {return [y[0], x]}
+            };
+            let i;
+            orientation === 'x' ? (dimension = startCoord[0]) : (dimension = startCoord[1])
+            for(i = dimension; i < (dimension + length); i++) {
+                if(i > 10) {throw new Error('Ship out of bounds')};
+                let newCoord = coord(orientation, i, startCoord);
+                positionArray.push(newCoord);
             }
-    }
-}
+            return positionArray;
+        };
+        const updateOccupiedArray = () => {
+            let posArray = createPositionArray();
+            occupiedArray.push({name, posArray})
+            return occupiedArray;
+        };
+        return updateOccupiedArray();
+    };
+    const receiveAttack = (coords) => {
+        for(let i = 0; i < occupiedArray.length; i++) {
+            let posString = JSON.stringify(occupiedArray[i].posArray);
+            if(posString.includes(JSON.stringify(coords))){
+                return coords;
+            }
+            missedArray.push(coords);
+        }
+    };
+    return {placeShip, receiveAttack};
 };
 
 
-module.exports = Ship;
+module.exports.Ship = Ship;
+module.exports.Gameboard = Gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,6 +1,23 @@
 const funcs = require('../src/index');
 const Gameboard = funcs.Gameboard;
 const newBoard = Gameboard();
-test('simple case', () => {
-  expect(newBoard.placeShip('tugger', 3, [1, 1], 'x')).toStrictEqual([{ 'name': 'tugger', 'posArray' : [[1,1],[2,1],[3,1]]}]);
+test('place a ship', () => {
+  expect(newBoard.placeShip('tugger', 3, [1, 1], 'x')).toMatchObject([
+    { 'name': 'tugger', 'posArray' : [[1,1],[2,1],[3,1]]}
+  ]);
+});
+
+test('place another ship', () => {
+  expect(newBoard.placeShip('trawler', 5, [2, 3], 'y')).toMatchObject([
+    { 'name': 'tugger', 'posArray' : [[1,1],[2,1],[3,1]]},
+    { 'name': 'trawler', 'posArray' : [[2,3],[2,4],[2,5],[2,6],[2,7]]}
+  ]);
+});
+
+test('reject ship if out of bounds', () => {
+  expect(() => newBoard.placeShip('cruiser', 5, [7, 7, 'x'])).toThrow('Ship out of bounds')
+});
+
+test('fdgff', () => {
+  expect(newBoard.receiveAttack([2,5])).toMatchObject([2,5])
 });
